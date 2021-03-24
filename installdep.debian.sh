73a3dbf01c65df5bd8a2bd4f31a263708f48de24
sodo <djdisodo@gmail.com>
2021-03-24 19:32:13 +0900

    add dependency
    Signed-off-by: sodo <djdisodo@gmail.com>

--- installdep.debian.sh
+++ installdep.debian.sh
@@ -1,2 +1,2 @@
 #install dependency for debian 10
-sudo apt install -y libssl-dev
+sudo apt install -y libssl-dev libglib2.0-dev libcairo2-dev libpango1.0-dev libatk1.0-dev libgdk-pixbuf-2.0-dev libgtk-3-dev
